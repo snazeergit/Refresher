4fd2b2d60fc5805788dfc05b9182d3aa4d27e578
snazeer <snazeer@DESKTOP-TS7O427>
2023-11-30 16:30:46 +0530

    updated

---
+++ CollectionsFramework/src/main/java/com/java/collections/list/Example.java
@@ -0,0 +1,19 @@
+package com.java.collections.list;
+
+
+
+public class Example {
+	
+	static int a = 10;
+	static int b = 20;
+	int x = 30;
+	int y = 40;
+
+	public static void main(String[] args) {
+		int p = 50;
+		int q = 60;
+		Example e1 = new Example();
+		Example e2 = new Example();
+	}
+
+}
