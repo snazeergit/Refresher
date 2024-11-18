7408ed720abc0ee718d7954e93cb7c43b7f12a76
Syed Nazeer <nazeersd.java@gmail.com>
2024-11-18 09:28:36 +0530

    upto date

---
+++ StringDeepDive/src/main/java/com/vmware/interview/Count_Repeated_Words_In_Given_String_v3.java
@@ -0,0 +1,29 @@
+package com.vmware.interview;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Count_Repeated_Words_In_Given_String_v3 {
+
+	public static void main(String[] args) {
+		String str = "This is 		a test  sentence 	and this test   IS just a TEST";
+		String[] words = str.toLowerCase().split("\\s+");
+		List<String> list = new ArrayList<String>(Arrays.asList(words));
+
+		list.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream()
+				.filter(entry -> entry.getValue() > 1).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
+				.forEach((k, v) -> {
+					System.out.println(k + " -> has repeated " + v + " times");
+				});
+
+	
+
+	}
+
+}
